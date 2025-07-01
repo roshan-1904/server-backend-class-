b18a6b261feb02dfe3769ff5d91a87b5e3ec6cde
roshan-1904 <gowthamjoshav@gmail.com>
2025-07-01 21:41:49 +0530

    add the update delete search

--- mogo.js
+++ mogo.js
@@ -30,6 +30,48 @@ app.get('/get',async (req,res) => {
        res.status(500).json({message:error.message}) 
     }
 })
+// get data
+app.get("/get/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//  delete the data
+app.delete("/delete/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//  Update the ID
+app.put("/update/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User updated", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 mongoose.connect("mongodb://localhost:27017/project",{
    
